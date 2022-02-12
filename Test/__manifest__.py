74dcf80fd816c7f8e3508959ebab234f85d38273
dharmishthagojiya <dharmishtha.1into2digital@gmail.com>
2022-02-12 21:34:58 +0530

    new

--- Test/__manifest__.py
+++ Test/__manifest__.py
@@ -2,11 +2,10 @@
     'name': 'TestModule',
     'sequence': -100,
     'depends': ['base','mail'],
-    'data': ['security/ir.model.access.csv', 
+    'data': ['security/ir.model.access.csv',
+             'wizard/create_offer_view.xml',
              'views/estate_view.xml',
              'views/property_type.xml',
-             'views/user.xml',
-             'wizard/create_offer.py'
-             ],
+             'views/user.xml' ],
     'application': 'True'
 }
--- Test/wizard/create_offer.py
+++ Test/wizard/create_offer.py
@@ -1,9 +1,11 @@
 from odoo import api,fields,models
 
 class CreateOfferWizard(models.TransientModel):
-     _name = "create.offer.wizard"  #estate_property
-     _description = "Test Model"
-    
+    _name = 'create.offer.wizard'
+    _description = "wizard of Offer"
 
-     name = fields.Char(string="Title", required=True, tracking=True)
-    
+    name = fields.Char(string="name",required=True)
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
+
+    def action_create_offer(self):
+        print("button clicked")
