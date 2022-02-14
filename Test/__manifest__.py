55b8961b9dfac4ef474e2d1e6966900767b50cce
dharmishthagojiya <dharmishtha.1into2digital@gmail.com>
2022-02-14 18:08:06 +0530

    reportcreate

--- Test/__manifest__.py
+++ Test/__manifest__.py
@@ -3,9 +3,15 @@
     'sequence': -100,
     'depends': ['base','mail'],
     'data': ['security/ir.model.access.csv',
-             'wizard/create_offer_view.xml',
              'views/estate_view.xml',
              'views/property_type.xml',
-             'views/user.xml' ],
+             'views/user.xml',
+             "report/estate_reports.xml",
+             "report/estate_report_views.xml",
+             'wizard/create_offer_view.xml',
+             ],
+    "demo": [
+        "data/estate_demo.xml"
+    ],
     'application': 'True'
 }
--- Test/wizard/create_offer.py
+++ Test/wizard/create_offer.py
@@ -4,8 +4,48 @@ class CreateOfferWizard(models.TransientModel):
     _name = 'create.offer.wizard'
     _description = "wizard of Offer"
 
-    name = fields.Char(string="name",required=True)
+    name = fields.Char(string="name", required=True)
     property_type_id = fields.Many2one("estate.property.type", string="Type")
 
     def action_create_offer(self):
         print("button clicked")
+'''from odoo import api, fields, models
+
+
+class CreateOfferWizard(models.TransientModel):
+    _name = 'create.offer.wizard'
+    _description = "wizard of Offer"
+
+    amount = fields.Float(string="Price")
+    status = fields.Selection(string="Status", selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
+    # partner_id = fields.Many2one('res.partner', required=True)
+    validity = fields.Integer(string="Validity (days)", default="7")
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date", inverse="_inverse_date")
+
+    def action_create_offer(self):
+             vals = {
+             'price': self.price,
+             'validity': self.validate,
+             'partner_id':self.partner_id,
+             'date_deadline':self.date_deadline,
+             'status':self.status
+             }
+             print("vals....", vals)
+             self.env['estate.property.offer'].Create(vals)
+
+    @api.depends("validity")
+    def _compute_date(self):
+        for rec in self:
+            rec.date_deadline = fields.Datetime.add(rec.create_date.date(), days=rec.validity)
+
+    def _inverse_date(self):
+        fmt = '%Y-%m-%d'
+        for rec in self:
+            d1 = fields.datetime.strptime(str(rec.date_deadline), fmt)
+            d2 = fields.datetime.strptime(str(rec.create_date.date()), fmt)
+            diff = (d1 - d2).days
+            rec.validity = str(diff)
+
+
+
+'''
